60d21975dc58aed7defd7e5494761d1b94a3bc9e
time-killer-games <samuelvenable@hotmail.com>
2020-10-03 10:13:52 -0400

    Add files via upload

--- VideoPlayer (FreeBSD x64).sh
+++ VideoPlayer (FreeBSD x64).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "VideoPlayer (x64)"
 mkdir "VideoPlayer (x64)/FreeBSD"
-g++ "VideoPlayer.cpp" "VideoPlayer/videoplayer.cpp" -o "VideoPlayer (x64)/FreeBSD/libvidplayer.so" -std=c++17 -shared `pkg-config --libs --cflags mpv` -lX11 -static-libgcc -static-libstdc++ -fPIC -m64
+g++ "VideoPlayer.cpp" "VideoPlayer/videoplayer.cpp" -o "VideoPlayer (x64)/FreeBSD/libvidplayer.so" -std=c++17 -shared `pkg-config --libs --cflags mpv` -static-libgcc -static-libstdc++ -fPIC -m64
